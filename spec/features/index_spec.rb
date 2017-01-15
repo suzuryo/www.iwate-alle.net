5d3083925164b07789c8c2b378975c80d1968098
suzuryo <suzuryo@gmail.com>
2017-01-15 20:40:35 +0900

    spec index.html

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -5,14 +5,26 @@ describe 'index', type: :feature do
     visit '/'
   end
 
-  it 'has the correct title header' do
+  it 'has the correct title' do
     expect(page).to have_selector 'h1'
-    within 'h1' do
-      expect(page).to have_content 'Home'
+    within '.welcome > h1' do
+      expect(page).to have_title '岩手県内の各地域のアレルギー患者の親の会「いわてアレルギーの会」'
+    end
+  end
+
+
+  it 'has the correct header' do
+    expect(page).to have_selector 'h1'
+    within '.welcome > h1' do
+      expect(page).to have_content 'いわてアレルギーの会'
     end
   end
 
   it 'has a article in there too' do
     expect(page).to have_selector 'article#article_index'
+  end
+
+  it 'has a body class' do
+    expect(page).to have_selector 'body#index'
   end
 end
