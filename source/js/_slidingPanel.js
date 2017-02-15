8e8c4dce1be22d21c0e5ec4a7fae06d7275a860b
suzuryo <suzuryo@gmail.com>
2017-02-15 09:13:10 +0900

    a.nav-trigger   .proejct-open

--- source/js/_slidingPanel.js
+++ source/js/_slidingPanel.js
@@ -5,6 +5,7 @@ const page = require('page');
 const resetClass = () => {
   $('.summary > li').removeClass('slide_out');
   $('.contents > li').removeClass('selected', 'content-visible');
+  $('a.nav-trigger').removeClass('project-open');
 };
 
 const slideOut = () => {
@@ -20,24 +21,28 @@ const test1 = (e) => {
       resetClass();
       slideOut();
       $('.contents > li.goal').addClass('selected', 'content-visible');
+      $('a.nav-trigger').addClass('project-open');
       break;
     case 'activities':
       $('.summary li.activities div.p_t').hide();
       resetClass();
       slideOut();
       $('.contents > li.activities').addClass('selected', 'content-visible');
+      $('a.nav-trigger').addClass('project-open');
       break;
     case 'line_at':
       $('.summary li.line_at div.p_t').hide();
       resetClass();
       slideOut();
       $('.contents > li.line_at').addClass('selected', 'content-visible');
+      $('a.nav-trigger').addClass('project-open');
       break;
     case 'network':
       $('.summary li.network div.p_t').hide();
       resetClass();
       slideOut();
       $('.contents > li.network').addClass('selected', 'content-visible');
+      $('a.nav-trigger').addClass('project-open');
       break;
     default:
       $('.summary div.p_t').show();
