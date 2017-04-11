2589369eb2f66ea98fcc7ada60a617e321fb0248
suzuryo <suzuryo@gmail.com>
2017-04-11 14:00:05 +0900

    pre open

--- source/js/_slidingPanel.js
+++ source/js/_slidingPanel.js
@@ -6,6 +6,7 @@ const resetClass = () => {
   $('.summary > li').removeClass('slide_out');
   $('.contents > li').removeClass('selected', 'content-visible');
   $('a.nav-trigger').removeClass('project-open', 'nav-visible');
+  $('div.container').removeClass('project-open');
 };
 
 const slideOut = () => {
@@ -20,29 +21,33 @@ const test1 = (e) => {
       $('.summary li.goal div.p_t').hide();
       resetClass();
       slideOut();
-      $('.contents > li.goal').addClass('selected', 'content-visible');
+        $('ul.contents > li.goal').addClass('selected', 'content-visible');
       $('a.nav-trigger').addClass('project-open');
+      $('div.container').addClass('project-open');
       break;
     case 'activities':
       $('.summary li.activities div.p_t').hide();
       resetClass();
       slideOut();
-      $('.contents > li.activities').addClass('selected', 'content-visible');
+      $('ul.contents > li.activities').addClass('selected', 'content-visible');
       $('a.nav-trigger').addClass('project-open');
+      $('div.container').addClass('project-open');
       break;
     case 'disaster':
       $('.summary li.disaster div.p_t').hide();
       resetClass();
       slideOut();
-      $('.contents > li.disaster').addClass('selected', 'content-visible');
+      $('ul.contents > li.disaster').addClass('selected', 'content-visible');
       $('a.nav-trigger').addClass('project-open');
+      $('div.container').addClass('project-open');
       break;
     case 'network':
       $('.summary li.network div.p_t').hide();
       resetClass();
       slideOut();
-      $('.contents > li.network').addClass('selected', 'content-visible');
+      $('ul.contents > li.network').addClass('selected', 'content-visible');
       $('a.nav-trigger').addClass('project-open');
+      $('div.container').addClass('project-open');
       break;
     case 'primary-nav':
       $('a.nav-trigger').addClass('nav-visible');
@@ -58,6 +63,25 @@ const test1 = (e) => {
 
 page('*', test1);
 page({hashbang:true});
+
+$(() => {
+  $('a.nav-trigger').on('click', () => {
+    if (!$('a.nav-trigger').hasClass('project-open')) {
+      $('a.nav-trigger').toggleClass('nav-visible');
+      $('#primary-nav').toggleClass('nav-visible');
+    } else {
+    }
+  });
+
+  $('#primary-nav ul li a').on('click', () => {
+    if (!$('a.nav-trigger').hasClass('project-open')) {
+      $('a.nav-trigger').toggleClass('nav-visible');
+      $('#primary-nav').toggleClass('nav-visible');
+    } else {
+    }
+  });
+
+});
 
 module.exports = {
   slidingPanel: 'slidingPanel',
